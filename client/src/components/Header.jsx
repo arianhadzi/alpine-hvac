c5e905bf8a52a3faf5b6e0188956c18fcf3175db
arianhadzi <90879508+arianhadzi@users.noreply.github.com>
2025-12-01 20:10:16 -0500

    header work

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,19 @@
 import "./Header.css";
+import logo from "../assets/react.svg";
 
 function Header() {
   return (
     <header className="site-header">
       <div className="header-inner">
+
+        {/* left logo */}
+        <div className="header-left">
+          <a href="#home" className="header-logo">
+            <img src={logo} alt="company logo" />
+          </a>
+        </div>
+
+        {/* middle */}
         <nav className="header-nav">
           <a href="#home">Home</a>
           <a href="#about">About Us</a>
@@ -11,11 +21,13 @@ function Header() {
           <a href="#heating">Heating</a>
         </nav>
 
+        {/* right */}
         <div className="header-actions">
           <a href="#request-service" className="header-cta">
             Request Service
           </a>
         </div>
+
       </div>
     </header>
   );
